36a22bd4de0c9ac1cf822c146e66e0592e71da03
Victor <victor.makeaveli@gmail.com>
2020-10-10 11:59:24 -0300

    testing

--- polls/models.py
+++ polls/models.py
@@ -8,8 +8,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    
     def __str__(self):
         return self.question_text
 
@@ -19,12 +23,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.choice_text
