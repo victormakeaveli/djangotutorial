c4da2b1b11c09bae268423e31a4a9f3ae870dc49
Victor <victor.makeaveli@gmail.com>
2020-11-08 14:59:01 -0300

    major changes

--- polls/views.py
+++ polls/views.py
@@ -50,12 +50,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-        
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5] 
